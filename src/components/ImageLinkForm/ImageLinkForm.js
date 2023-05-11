7e2150ba167738d8447a098f397d69782a1e033a
Benjamin Abbott <ben_abbott@outlook.com>
2023-05-11 11:32:04 +0100

    adding default value for url input

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import './ImageLinkForm.css'
 
 
 
 
 const ImageLinkForm = ({ onInputChange, onImageSubmit, onClear, input }) => {
+    const ref = useRef(null);
     return (
         <div>
             <p className='f3'>
                 {'This Magic Brain Will Find Faces in Your Pictures. Give it a Try.'}
             </p>
             <div className='center w-50 pa4 br3 shadow-5 form'>
-                <input type='text' className='f4 pa2 w-70 center' value={input} onChange={onInputChange} />
+                <input ref={ref} defaultValue="Give me the URL of an image" type='text' className='f4 pa2 w-70 center' value={input} onChange={onInputChange} />
                 <button className='w-25 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onImageSubmit}>Detect</button>
                 <button className='w-15 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onClear}>Clear</button>
             </div>
